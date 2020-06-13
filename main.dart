a8567ab6ad4f5ea029a6428f04172bbc2ffb2137
ananya2712 <55040997+ananya2712@users.noreply.github.com>
2020-06-14 01:22:07 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,277 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+  home: Article() ,
+));
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class Article extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "User Profile",
-      debugShowCheckedModeBanner: false,
-      home: UserProfilePage(),
-    );
-  }
-}
-
-class UserProfilePage extends StatefulWidget {
-  @override
-  _UserProfilePageState createState() => _UserProfilePageState();
-}
-
-class _UserProfilePageState extends State<UserProfilePage> {
-  final String _fullName = "Ananya Uppal";
-
-  final String _status = "Vocal Musician";
-
-  final String _bio =
-      "\"Hi.I am a Hindustani Vocal Musician with a Senior Diploma.Contact for details about next e-concert and classes.\"";
-
-  final String _specialization="Hindustani";
-
-  final String _posts = "100";
-
-  final String _likes = "500";
-
-  final String _level="Artist" ;
-
-  Widget _buildCoverImage(Size screenSize) {
-    return Container(
-      height: screenSize.height / 3.0,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/flower.jpeg'),
-          fit: BoxFit.cover,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildProfileImage() {
-    return Center(
-      child: Container(
-        width: 140.0,
-        height: 140.0,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/girl.jpg'),
-            fit: BoxFit.cover,
-          ),
-          borderRadius: BorderRadius.circular(80.0),
-          border: Border.all(
-            color: Colors.white,
-            width: 10.0,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildFullName() {
-    TextStyle _nameTextStyle = TextStyle(
-      color: Colors.black,
-      fontSize: 28.0,
-      fontWeight: FontWeight.w700,
-    );
-
-    return Text(
-      _fullName,
-      style: _nameTextStyle,
-    );
-  }
-
-  Widget _buildStatus(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 6.0),
-      decoration: BoxDecoration(
-        color: Theme.of(context).scaffoldBackgroundColor,
-        borderRadius: BorderRadius.circular(4.0),
-      ),
-      child: Text(
-        _status,
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 20.0,
-          fontWeight: FontWeight.w300,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildStatItem(String label, String count) {
-    TextStyle _statLabelTextStyle = TextStyle(
-      color: Colors.black,
-      fontSize: 16.0,
-      fontWeight: FontWeight.w200,
-    );
-
-    TextStyle _statCountTextStyle = TextStyle(
-      color: Colors.black54,
-      fontSize: 24.0,
-      fontWeight: FontWeight.bold,
-    );
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          count,
-          style: _statCountTextStyle,
-        ),
-        Text(
-          label,
-          style: _statLabelTextStyle,
-        ),
-      ],
-    );
-  }
-
-  Widget _buildStatContainer() {
-    return Container(
-      height: 60.0,
-      margin: EdgeInsets.only(top: 8.0),
-      decoration: BoxDecoration(
-        color: Color(0xFFEFF4F7),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          _buildStatItem("Specialization", _specialization),
-          _buildStatItem("Posts", _posts),
-          _buildStatItem("Likes", _likes),
-          _buildStatItem("Level", _level),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildBio(BuildContext context) {
-    TextStyle bioTextStyle = TextStyle(
-      fontFamily: 'Spectral',
-      fontWeight: FontWeight.w400,//try changing weight to w500 if not thin
-      fontStyle: FontStyle.italic,
-      color: Color(0xFF799497),
-      fontSize: 16.0,
-    );
-
-    return Container(
-      color: Theme.of(context).scaffoldBackgroundColor,
-      padding: EdgeInsets.all(8.0),
-      child: Text(
-        _bio,
-        textAlign: TextAlign.center,
-        style: bioTextStyle,
-      ),
-    );
-  }
-
-  Widget _buildSeparator(Size screenSize) {
-    return Container(
-      width: screenSize.width / 1.6,
-      height: 2.0,
-      color: Colors.black54,
-      margin: EdgeInsets.only(top: 4.0),
-    );
-  }
-
-  Widget _buildUpcomingConcerts(BuildContext context) {
-    return Container(
-      color: Theme.of(context).scaffoldBackgroundColor,
-      padding: EdgeInsets.only(top: 8.0),
-      child: Text(
-        "Upcoming E-CONCERT: 15th June 2020 , 5 pm",
-        style: TextStyle( fontSize: 16.0,color:Colors.purpleAccent,),
-      ),
-    );
-  }
-
-  Widget _buildButtons(){
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        RaisedButton(
-          onPressed: (){},
-          child:Text("BUY TICKETS HERE"),
-          color:Colors.grey[500],
-        ),
-        SizedBox(width:20.0),
-        RaisedButton(
-          onPressed: (){} ,
-          child:Text('MESSAGE ME HERE'),
-          color:Colors.blueGrey,
-        ),
-      ],
-    );
-  }
-
-  Widget _buildContact(){
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 16.0),
-      child:Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-       children: <Widget>[
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height:100),
             Center(
-              child: Text('Phone Number : 987654321',
-                style:TextStyle(
-                  fontSize: 20.0,
-                  color:Colors.grey[800],
-                  letterSpacing: 0.75,
-                ),
-              ),
-            ),
-         SizedBox(height:12.0),
-            Center(
-              child: Text('Email Address: ananya123@gmail.com',
-                  style:TextStyle(
-                    fontSize: 20.0,
-                    color:Colors.grey[800],
-                    letterSpacing: 0.75,
+              child: Text('HINDUSTANI MUSIC HISTORY',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.black87,
+                    letterSpacing: 2.0,
                   )
               ),
-            )
-       ],
-      ),
-    );
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          _buildCoverImage(screenSize),
-          SafeArea(
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: screenSize.height / 6.4),
-                  _buildProfileImage(),
-                  _buildFullName(),
-                  _buildStatus(context),
-                  _buildStatContainer(),
-                  _buildBio(context),
-                  _buildSeparator(screenSize),
-                  SizedBox(height: 10.0),
-                  _buildUpcomingConcerts(context),
-                  SizedBox(height: 8.0),
-                  _buildButtons(),
-                  SizedBox(height: 10.0),
-                  _buildSeparator(screenSize),
-                  _buildContact(),
-                ],
-              ),
             ),
-          ),
-        ],
+            SizedBox(height: 20.0),
+            Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit.'),
+            Text('Quisque nec ornare lorem. Maecenas maximus auctor tortor, et eleifend lorem ultricies nec.'),
+            Text('Sed vitae urna sit amet mi lobortis efficitur. Aliquam ac lobortis tellus, sed placerat ipsum.'),
+            Text('Vivamus viverra eleifend porta. Mauris convallis metus et arcu hendrerit lacinia.'),
+            Text('Duis purus sapien, sollicitudin eu rhoncus vel, placerat at turpis. Fusce at laoreet neque.'),
+            Text('Donec faucibus, leo et maximus elementum, nulla diam condimentum leo, eget consectetur massa nunc non mauris.'),
+            Text('Vestibulum vel efficitur urna, sit amet fermentum leo. Praesent tellus leo, congue eget tincidunt ut.'),
+            Text('Maecenas vulputate lacus nisl, vitae iaculis eros faucibus vitae. Nulla maximus congue sem,'),
+            Text('ac blandit nulla efficitur cursus. Mauris nulla urna, sodales eget fermentum in,'),
+            Text('Donec sit amet ligula ex. Proin aliquet justo sed arcu bibendum consequat.'),
+            Text('Nulla facilisi. Duis sollicitudin enim eros, at consequat ex fermentum id.'),
+            Text('Morbi eu risus porta, varius purus eu, accumsan sem. Phasellus placerat elementum mi'),
+            Text('sit amet semper arcu suscipit sit amet. Nulla facilisi'),
+          ],
+        ),
       ),
     );
   }
 }
+
